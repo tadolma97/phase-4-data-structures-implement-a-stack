5282fe0657f03057d6b4269f1c9ac35e55e53b3d
Tashi Dolma <tadolma97@gmail.com>
2022-08-09 18:31:20 -0400

    commit tree c868d0fa0ab90ab4611050e4631a126847847d5e

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,41 @@
 # your code here
+class Stack 
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+    def push(value)
+        if @stack.length == @limit
+            raise "Stack Overflow"
+        else 
+            @stack.push(value)
+        end
+        
+    end
+    def pop
+        @stack.pop
+    end
+    def peek
+        @stack.last
+    end
+    def size
+        @stack.length
+    end
+    def empty?
+        @stack.empty?
+    end
+    def full?
+        if @stack.length == @limit
+            return true
+        else 
+            return false
+        end
+    end
+    def search(value)
+        @stack.each do |num|
+            return @stack.length -[num] - 1 if value==num
+        end
+        -1
+    end
+
+end
